3361e71b50d64346a23bc91b2ad61203522e01e3
andrey-git <andrey-git@users.noreply.github.com>
2013-09-19 20:19:23 +0200

    Remove input checks.

--- v1.c
+++ v1.c
@@ -1,18 +1,17 @@
 /* === V1 ===                                                                 */
 /* Simple and optimized for space. No extra memory is allocated.              */
-/* Returns NULL on illegal input and on allocation errors.                    */
+/* Returns NULL on allocation errors.                                         */
 /* Best usecase: Short strings or string with large variance of sizes.        */
 
 #include <stdlib.h>
 #include <string.h>
 
 void str_free(char** ptr) {
-  if (ptr) free(*ptr);
+  free(*ptr);
 }
  
 char* str_cpy(char** dest, const char* source) {
-  if (!dest) return NULL;
-  if (source && *dest != source) {
+  if (source) {
     size_t src_size = strlen(source) + 1;
     char* result = malloc(src_size);
     if (!result) return NULL;
@@ -24,7 +23,6 @@ char* str_cpy(char** dest, const char* source) {
 }
  
 char* str_cat(char** dest, const char* source) {
-  if (!dest) return NULL;
   if (!*dest) return str_cpy(dest, source);
   if (source) {
     size_t src_size = strlen(source) + 1;
